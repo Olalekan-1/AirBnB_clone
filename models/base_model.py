099de0d627b46b3bf46c708bf1133cfb928ddac7
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-07-13 13:32:43 +0100

    revert merges

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
     def to_dict(self):
         """Return a dictionary of instance attributes
@@ -24,7 +24,7 @@ class BaseModel:
         the dictionary is a key/value pair of attribute name/value
         include a key "__class__" with a value of instance class name
         """
-        model_dict = {'__class__': self.__class__.__name__}
+        model_dict = {'__class__': type(self).__name__}
         for key, value in self.__dict__.items():
             model_dict[key] = value
         model_dict['created_at'] = model_dict['created_at'].isoformat()
@@ -35,7 +35,6 @@ class BaseModel:
         """Set updated_at attribute to current datetime
         """
         self.updated_at = datetime.now()
-        return self.updated_at
 
     def __str__(self):
         """Return String representation of object
